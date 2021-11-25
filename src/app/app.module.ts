ab62e8d2a4d1686ab71c874afe50db7952110ed6
Alberto Benitez <albertobenitezbahena@gmail.com>
2021-11-25 17:52:20 -0500

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegistrarInmuebleComponent } from './registrar-inmueble/registrar-inmueble.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    RegistrarInmuebleComponent
   ],
   imports: [
     BrowserModule,
